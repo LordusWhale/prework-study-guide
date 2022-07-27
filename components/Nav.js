3f17bdfc7940a5434c8b857c7e1a98c25f9bc499
Sam Sweeney <samsweeneyy@gmail.com>
2022-07-27 22:36:55 +1000

    Added a random study generator. Fixed links in nav

--- components/Nav.js
+++ components/Nav.js
@@ -5,26 +5,30 @@ import Link from "next/link"
 export const Nav = () =>{
     return(
         <nav className="flex w-full h-28 bg-black justify-between items-center">
-            <h1 className="text-white font-bold text-3xl text-center p-10">Study Guide</h1>
-            <ul className="text-white pr-20">
-                <a href="#" className="p-4 m-6 text-lg rounded-lg px-7 duration-500 hover:bg-red-900 ">
-                <Link href="#">HTML</Link>
-                </a>
-                <a href="#" className="p-4 m-6 text-lg rounded-lg px-7 duration-500 hover:bg-red-900 ">
-                <Link href="#">CSS</Link>
-                </a>
-                <a href="#" className="p-4 m-6 text-lg rounded-lg px-7 duration-500 hover:bg-red-900 ">
-                <Link href="#">JAVASCRIPT</Link>
-                </a>
-                <a href="#" className="p-4 m-6 text-lg rounded-lg px-7 duration-500 hover:bg-red-900 ">
-                <Link href="#">GIT</Link>
-                </a>
+            <Link href="/" className="text-white font-bold text-3xl text-center p-10">
+                <a className="text-white font-bold text-3xl text-center p-10">STUDY GUIDE</a>
+            </Link>
+           
+            
+            <div className="text-white pr-20">
+                <Link href="#" className="p-4 m-6 text-lg rounded-lg px-7 duration-500 hover:bg-red-900">
+                    <a className="p-4 m-6 text-lg rounded-lg px-7 duration-500 hover:bg-red-900 ">HTML</a>
+                </Link>
+                <Link href="#" className="p-4 m-6 text-lg rounded-lg px-7 duration-500 hover:bg-red-900">
+                    <a className="p-4 m-6 text-lg rounded-lg px-7 duration-500 hover:bg-red-900 ">CSS</a>
+                </Link>
+                <Link href="#" className="p-4 m-6 text-lg rounded-lg px-7 duration-500 hover:bg-red-900">
+                    <a className="p-4 m-6 text-lg rounded-lg px-7 duration-500 hover:bg-red-900 ">JAVASCRIPT</a>
+                </Link>
+                <Link href="#" className="p-4 m-6 text-lg rounded-lg px-7 duration-500 hover:bg-red-900">
+                    <a className="p-4 m-6 text-lg rounded-lg px-7 duration-500 hover:bg-red-900 ">GIT</a>
+                </Link>
                 
                 
-            </ul>
-            <a href="#" className="p-4 text-white text-lg rounded-lg mr-20 duration-500 bg-red-900 hover:translate-y-1 ">
-                <Link href="#">Login</Link>
-            </a>
+            </div>
+            <Link href="#" className="p-4 text-white text-lg rounded-lg mr-20 duration-500 bg-red-900 hover:translate-y-1 ">
+                <a className="p-4 text-white text-lg rounded-lg mr-20 duration-500 bg-red-900 hover:translate-y-1 ">LOGIN</a>
+            </Link>
         </nav>
     )
 }
