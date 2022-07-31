efd3d13bb929ec4aac80c644cdf56d284b5ab3fe
Sam Sweeney <samsweeneyy@gmail.com>
2022-07-31 17:41:46 +1000

    Updated sign in modal with google sign in png

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,5 @@
 import { login } from "../firebase/functions";
-const googleSignIn = require('../public/google.png');
+import Image from '../public/google-sign-in.png';
 export const Login = ({ setModalOn}) =>{
     const handleOKClick = async () =>{
         await login();
@@ -8,16 +8,17 @@ export const Login = ({ setModalOn}) =>{
 
     const handleCancleClick = () =>{
         setModalOn(false)
+        console.log(Image)
     }
     return(
     <div className="bg-zinc-900 opacity-90 fixed inset-0 z-50">
         <div className="flex h-screen justify-center items-center">
-            <div className="flex-col justify-center bg-white py-44 px-44 border-4 rounded-xl ">
-                <div className="flex text-lg text-black mb-10">Sign in with Google</div>
+            <div className="flex-col justify-center bg-gray-700 py-20 px-20 border-4 rounded-xl ">
+               
                 <div className="flex flex-col gap-3">
+                   
                     <button 
-                        className="rounded px-4 py-2 text-white bg-green-400"
-                        onClick={()=>handleOKClick()}>Sign in with Google</button>
+                        onClick={()=>handleOKClick()}> <img className="w-48" src={Image.src}/></button>
                         
                     
                     <button className="rounded px-4 py-2 text-white bg-blue-500"
