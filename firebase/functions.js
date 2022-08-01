c5079b6cbe9dd5ddb47fe0986c459f13ad932323
Sam Sweeney <samsweeneyy@gmail.com>
2022-08-01 14:57:48 +1000

    Added redirect to home page after login

--- firebase/functions.js
+++ firebase/functions.js
@@ -18,6 +18,7 @@ export async function login() {
             } catch (e) {
                 console.error("Error adding document: ", e);
             }
+            window.location.href = "/"
 
         }).catch((error) => {
             const credential = GoogleAuthProvider.credentialFromError(error);
