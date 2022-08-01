060eb778a6f34d85240c09c176914f1935053372
Sam Sweeney <samsweeneyy@gmail.com>
2022-08-01 13:57:19 +1000

    Changed auth domain key

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
 
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
   
-    authDomain: process.env.NEXT_PUBLIC_VERCEL_URL,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
   
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   
