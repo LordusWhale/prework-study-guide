bd81ccfd30ebb64df021866ba6a89f628f29c625
Sam Sweeney <samsweeneyy@gmail.com>
2022-08-02 16:35:22 +1000

    Fixed meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="A simple website to create, update and delete study cards." />
       </Head>
